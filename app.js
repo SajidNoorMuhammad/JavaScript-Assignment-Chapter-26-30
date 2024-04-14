8bb0c74588e7c0aeff60861a13d2ca6f03d865ba
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-04-14 17:09:56 +0500

    Add Question 1.

--- app.js
+++ app.js
@@ -1,1 +1,62 @@
 // Question # 01
+var userInput= prompt ("Enter a Positive Number")
+var roundoff= Math.round(userInput)
+var floor= Math.floor(userInput)
+var ceil= Math.ceil(userInput)
+
+// document.write(`number: ${userInput} </br> round off value: ${roundoff} <br> floor value: ${floor} <br> ceil value: ${ceil}`)
+
+// Question # 02
+// var userInput= prompt ("Enter a Negative Number")
+// var roundoff= Math.round(userInput)
+// var floor= Math.floor(userInput)
+// var ceil= Math.ceil(userInput)
+
+// document.write(`number: ${userInput} </br> round off value: ${roundoff} <br> floor value: ${floor} <br> ceil value: ${ceil}`)
+
+// Question # 03
+// var userInput= prompt("Enter Any Number")
+// var absolute= Math.abs(userInput)
+
+// document.write(`The absolute Value of ${userInput} is ${absolute}`)
+
+// Question # 04
+// var number= Math.floor (Math.random() * 3)
+// var second= Math.floor ((Math.random() * 4) + 1)
+
+// document.write(`Random dice value: ${number} <br> Random dice value: ${second}`)
+
+// Question # 05
+// var userInput= Math.floor(Math.random()* 3)
+// var second= Math.floor(Math.random()* 2)
+
+
+// if (userInput >= second){
+//     document.write(`${userInput} <br> Random Coin Value: Heads <br> ${second} <br> Random Coin Value: Tails`)
+// }else {
+//     document.write("SOOOORRRRYYYY")
+// }
+
+// Question # 06
+// var number = Math.floor((Math.random() * 100) + 1);
+
+// document.write(`random number between 1 and 100: ${number}`)
+
+// Question # 07
+//ask in class
+
+// Question # 08
+// var secretNum= Math.floor(Math.random()*10)
+// var userInput= prompt("Enter Any Number")
+
+// document.write(`<h1>Find The Secret Number Game</h1>`)
+// if (userInput == secretNum){
+//     document.write(`Secret Number: ${secretNum} <br> Input Number: ${userInput} <br> <h2>"Wohoo! You Find a Secret Num"<h2>`)
+// }else {
+//     document.write(`Secret Number: ${secretNum} <br> Input Number: ${userInput} <br> <h2>"Soory, Better Luck Next Time!"<h2>`)
+// }
+
+
+
+
+
