b01bd614c2e72c06aebf5a0320b8f64b9ae57533
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-04-14 17:10:30 +0500

    Add Question 01 result.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var roundoff= Math.round(userInput)
 var floor= Math.floor(userInput)
 var ceil= Math.ceil(userInput)
 
-// document.write(`number: ${userInput} </br> round off value: ${roundoff} <br> floor value: ${floor} <br> ceil value: ${ceil}`)
+document.write(`number: ${userInput} </br> round off value: ${roundoff} <br> floor value: ${floor} <br> ceil value: ${ceil}`)
 
 // Question # 02
 // var userInput= prompt ("Enter a Negative Number")
